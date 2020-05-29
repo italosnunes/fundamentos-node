47e97d1cffa658ddd41f0d368758cc220252dbfa
italosnunes <46608041+italosnunes@users.noreply.github.com>
2020-05-29 20:14:11 -0300

    finalize

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,35 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private repository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+    this.repository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid!');
+    }
+    const { total } = this.repository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new Error('You do not have enough balance!');
+    }
+
+    const transaction = this.repository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
